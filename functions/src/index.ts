40f731621b6ee9844fbdfe3faebaed56e243bf78
EthanWakeford <ethanwakeford@gmail.com>
2024-02-05 23:12:22 -0600

    this is killing me

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,9 @@
 // import axios, { AxiosResponse } from 'axios';
 import { onDocumentWritten } from 'firebase-functions/v2/firestore';
 import { initializeApp } from 'firebase-admin/app';
+import { onRequest } from 'firebase-functions/v2/https';
 import { debug } from 'firebase-functions/logger';
-import * as admin from 'firebase-admin';
+// import * as admin from 'firebase-admin';
 import { defineString } from 'firebase-functions/params';
 import { v2beta3 } from '@google-cloud/tasks';
 
@@ -15,8 +16,20 @@ const LOCATION = 'us-central1';
 
 debug('does this run immediately????');
 initializeApp();
-const db = admin.firestore();
+// const db = admin.firestore();
 const tasksClient = new v2beta3.CloudTasksClient();
+
+// export const buildOnWritten = onDocumentWritten('test/**', async (event) => {
+export const buildMe = onRequest(async () => {
+  debug('hi im running');
+  const currentDate = new Date();
+  const thisTimestamp = Math.floor(currentDate.getTime() / 1000);
+  const toRunTimestamp = thisTimestamp + 1 * 60;
+
+  createBuildTask(toRunTimestamp)
+    .then((response) => debug('success', response.scheduleTime))
+    .catch((error: any) => debug('Error:', error));
+});
 
 export const buildOnWritten = onDocumentWritten('test/**', async (event) => {
   debug(GITHUB_REPO.value());
@@ -24,34 +37,34 @@ export const buildOnWritten = onDocumentWritten('test/**', async (event) => {
   const currentDate = new Date();
   const thisTimestamp = Math.floor(currentDate.getTime() / 1000);
 
-  // retrieve most recent timestamp in firebase
-  const recentTimestamp = await getMostRecentTimestamp();
+  // // retrieve most recent timestamp in firebase
+  // const recentTimestamp = await getMostRecentTimestamp();
 
-  if (recentTimestamp !== null) {
-    const timeDifference = Math.abs(thisTimestamp - recentTimestamp);
+  // if (recentTimestamp !== null) {
+  //   const timeDifference = Math.abs(thisTimestamp - recentTimestamp);
 
-    debug(
-      `this timestamp: ${thisTimestamp} recent timestamp${recentTimestamp}`
-    );
+  //   debug(
+  //     `this timestamp: ${thisTimestamp} recent timestamp${recentTimestamp}`
+  //   );
 
-    const fiveMinutesInSeconds = 5 * 60;
+  //   const fiveMinutesInSeconds = 5 * 60;
 
-    if (timeDifference <= fiveMinutesInSeconds) {
-      // quit to limit the number of build invocations
+  //   if (timeDifference <= fiveMinutesInSeconds) {
+  //     // quit to limit the number of build invocations
 
-      debug('quitting');
-      return;
-    }
-  }
+  //     debug('quitting');
+  //     return;
+  //   }
+  // }
 
-  // add current timestamp to firestore
-  debug('uploading timestamp');
-  await uploadTimestamp(thisTimestamp);
+  // // add current timestamp to firestore
+  // debug('uploading timestamp');
+  // await uploadTimestamp(thisTimestamp);
 
-  debug('wait finished');
+  // debug('wait finished');
 
   // run in 5 minutes from now
-  const toRunTimestamp = thisTimestamp + 1 * 60 * 1000;
+  const toRunTimestamp = thisTimestamp + 1 * 60;
 
   createBuildTask(toRunTimestamp)
     .then((response) => debug('success', response.scheduleTime))
@@ -60,6 +73,23 @@ export const buildOnWritten = onDocumentWritten('test/**', async (event) => {
 });
 
 const createBuildTask = async (toRunTimestamp: number) => {
+  debug('building task');
+
+  const updateQueueRequest = {
+    queue: {
+      name: `projects/${PROJECT_ID}/locations/${LOCATION}/queues/${QUEUE_NAME}`,
+      rateLimits: {
+        maxBurstSize: 1,
+        maxTasksDispatchedPerSecond: 1,
+        maxConcurrentTasks: 1,
+      },
+    },
+  };
+
+  await tasksClient.updateQueue(updateQueueRequest);
+
+  debug('queue updated');
+
   const body = {
     ref: 'main',
     inputs: {},
@@ -93,26 +123,27 @@ const createBuildTask = async (toRunTimestamp: number) => {
     task: task,
   };
   const [response] = await tasksClient.createTask(request);
+  debug('task added');
   return response;
 };
 
-const uploadTimestamp = async (timestamp: number) => {
-  // upload timestamp when run to firestore
-  const collection = db.collection('build_timestamps');
-  await collection.add({ timestamp });
-};
+// const uploadTimestamp = async (timestamp: number) => {
+//   // upload timestamp when run to firestore
+//   const collection = db.collection('build_timestamps');
+//   await collection.add({ timestamp });
+// };
 
-const getMostRecentTimestamp = async (): Promise<number | null> => {
-  // retrieve the most recent run timestamp
-  const snapshot = await db
-    .collection('build_timestamps')
-    .orderBy('timestamp', 'desc')
-    .limit(1)
-    .get();
+// const getMostRecentTimestamp = async (): Promise<number | null> => {
+//   // retrieve the most recent run timestamp
+//   const snapshot = await db
+//     .collection('build_timestamps')
+//     .orderBy('timestamp', 'desc')
+//     .limit(1)
+//     .get();
 
-  if (snapshot.docs.length === 0) return null;
+//   if (snapshot.docs.length === 0) return null;
 
-  const mostRecentTimestamp = snapshot.docs[0].get('timestamp');
+//   const mostRecentTimestamp = snapshot.docs[0].get('timestamp');
 
-  return mostRecentTimestamp;
-};
+//   return mostRecentTimestamp;
+// };
