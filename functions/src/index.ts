64d7a24bbee292e7232733b065934b4c2ba54f41
EthanWakeford <ethanwakeford@gmail.com>
2024-02-06 00:30:32 -0600

    listener isnt allowed to even look at the cloud tasks api for some reason

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,29 +1,27 @@
-// import axios, { AxiosResponse } from 'axios';
 import { onDocumentWritten } from 'firebase-functions/v2/firestore';
 import { initializeApp } from 'firebase-admin/app';
 import { onRequest } from 'firebase-functions/v2/https';
 import { debug } from 'firebase-functions/logger';
-// import * as admin from 'firebase-admin';
 import { defineString } from 'firebase-functions/params';
-import { v2beta3 } from '@google-cloud/tasks';
+import axios from 'axios';
 
 const GITHUB_REPO = defineString('GITHUB_REPO');
 const GITHUB_TOKEN = defineString('GITHUB_TOKEN');
 const WORKFLOW_ID = defineString('WORKFLOW_ID');
 const PROJECT_ID = defineString('PROJECT_ID');
 const QUEUE_NAME = defineString('QUEUE_NAME');
+const BUILD_URL = defineString('BUILD_URL');
 const location = 'us-central1';
 
 initializeApp();
-// const db = admin.firestore();
-const tasksClient = new v2beta3.CloudTasksClient();
 
-// export const buildOnWritten = onDocumentWritten('test/**', async (event) => {
 export const buildMe = onRequest(async (req, res) => {
-  debug('hi im running');
+  debug('hi im building');
   const currentDate = new Date();
   const thisTimestamp = Math.floor(currentDate.getTime() / 1000);
-  const toRunTimestamp = thisTimestamp + 1 * 60;
+
+  // run in 5 minutes
+  const toRunTimestamp = thisTimestamp + 5 * 60;
 
   await createBuildTask(toRunTimestamp)
     .then(() => {
@@ -36,47 +34,23 @@ export const buildMe = onRequest(async (req, res) => {
 });
 
 export const buildOnWritten = onDocumentWritten('test/**', async (event) => {
-  debug(GITHUB_REPO.value());
-  // get current time
-  const currentDate = new Date();
-  const thisTimestamp = Math.floor(currentDate.getTime() / 1000);
+  debug('hi document was written');
 
-  // // retrieve most recent timestamp in firebase
-  // const recentTimestamp = await getMostRecentTimestamp();
-
-  // if (recentTimestamp !== null) {
-  //   const timeDifference = Math.abs(thisTimestamp - recentTimestamp);
-
-  //   debug(
-  //     `this timestamp: ${thisTimestamp} recent timestamp${recentTimestamp}`
-  //   );
-
-  //   const fiveMinutesInSeconds = 5 * 60;
-
-  //   if (timeDifference <= fiveMinutesInSeconds) {
-  //     // quit to limit the number of build invocations
-
-  //     debug('quitting');
-  //     return;
-  //   }
-  // }
-
-  // // add current timestamp to firestore
-  // debug('uploading timestamp');
-  // await uploadTimestamp(thisTimestamp);
-
-  // debug('wait finished');
-
-  // run in 5 minutes from now
-  const toRunTimestamp = thisTimestamp + 1 * 60;
-
-  createBuildTask(toRunTimestamp)
-    .then(() => debug('success'))
-    .catch((error: any) => debug('Error:', error));
-  // debug(tasksClient);
+  axios
+    .get(BUILD_URL.value())
+    .then((response) => {
+      debug('Response:', response.data);
+    })
+    .catch((error) => {
+      debug('Error Caught:', error.message);
+    });
 });
 
 const createBuildTask = async (toRunTimestamp: number) => {
+  const { v2beta3 } = await import('@google-cloud/tasks');
+
+  const tasksClient = new v2beta3.CloudTasksClient();
+
   debug('building task');
 
   const getTasksRequest = {
@@ -88,23 +62,11 @@ const createBuildTask = async (toRunTimestamp: number) => {
   debug('got task list');
 
   // quit if task list isnt empty
-  if (tasklist.length > 0) return;
+  if (tasklist.length > 0) {
+    debug('build is already queued');
+    return;
+  }
   debug('queue is empty');
-
-  // const updateQueueRequest = {
-  //   queue: {
-  //     name: `projects/${PROJECT_ID.value()}/locations/${location}/queues/${QUEUE_NAME.value()}`,
-  //     rateLimits: {
-  //       maxBurstSize: 1,
-  //       maxTasksDispatchedPerSecond: 1,
-  //       maxConcurrentTasks: 1,
-  //     },
-  //   },
-  // };
-
-  // await tasksClient.updateQueue(updateQueueRequest);
-
-  // debug('queue updated');
 
   const body = {
     ref: 'main',
@@ -142,24 +104,3 @@ const createBuildTask = async (toRunTimestamp: number) => {
   debug('task added');
   return response;
 };
-
-// const uploadTimestamp = async (timestamp: number) => {
-//   // upload timestamp when run to firestore
-//   const collection = db.collection('build_timestamps');
-//   await collection.add({ timestamp });
-// };
-
-// const getMostRecentTimestamp = async (): Promise<number | null> => {
-//   // retrieve the most recent run timestamp
-//   const snapshot = await db
-//     .collection('build_timestamps')
-//     .orderBy('timestamp', 'desc')
-//     .limit(1)
-//     .get();
-
-//   if (snapshot.docs.length === 0) return null;
-
-//   const mostRecentTimestamp = snapshot.docs[0].get('timestamp');
-
-//   return mostRecentTimestamp;
-// };
