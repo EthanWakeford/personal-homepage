b7b4582f0d5ca9720d44aa454ddeeaeb038afecf
EthanWakeford <ethanwakeford@gmail.com>
2024-02-12 18:55:54 -0600

    lining up with components from other project

--- src/components/ProjectPageInfo.tsx
+++ src/components/ProjectPageInfo.tsx
@@ -14,13 +14,9 @@ const ProjectPageInfo: React.FC<Props> = ({
   credits,
 }) => {
   return (
-    <div className='px-8 py-16 lg:px-64 lg:py-32'>
-      <h1 className='text-center text-2xl'>{title}</h1>
-      <h3 className='text-md mb-16 mt-4 text-center lg:mb-32'>{company}</h3>
+    <div className='bg-dkbg2 px-8 py-16 lg:px-64 lg:py-32'>
       <div className='flex grid-cols-4 flex-col gap-16 lg:grid'>
-        <p className='break-words'>
-          {description}
-        </p>
+        <p className='col-span-3 break-words'>{description}</p>
         <div className='col-span-1 flex flex-col gap-4 text-center text-lg lg:text-left'>
           <h3 className=''>Credits</h3>
           <div className='flex flex-wrap justify-center gap-2 lg:justify-start'>
