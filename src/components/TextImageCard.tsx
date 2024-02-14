59b7976d2f622c523484c17afc2ab9acf943e104
EthanWakeford <ethanwakeford@gmail.com>
2024-02-13 20:27:11 -0600

    fixing project page

--- src/components/TextImageCard.tsx
+++ src/components/TextImageCard.tsx
@@ -18,18 +18,18 @@ const TextImagecard: React.FC<Props> = ({
   reversed = false,
 }) => {
   return (
-    <div className='grid h-[70vh] grid-rows-2 lg:h-[50vh] lg:grid-cols-3 lg:grid-rows-1'>
+    <div className='grid h-[70vh] max-w-full grid-rows-2 lg:h-[50vh] lg:grid-cols-3 lg:grid-rows-1 hover:shadow-inner transition-all'>
       <img
         src={imageLink}
         alt={imageAltText}
-        className={`${reversed && 'lg:order-1'}  h-full w-full object-cover lg:col-span-2`}
+        className={`${reversed && 'lg:order-1'} from-dkbg1 box-border h-full w-full object-cover lg:col-span-2`}
       />
-      <div className='flex flex-col justify-between px-4 py-8 text-center lg:col-span-1 lg:px-12 lg:py-24'>
+      <div className='flex flex-col justify-between bg-ltbg2 py-8  text-center text-black lg:col-span-1 lg:px-12 lg:py-24 dark:bg-dkbg1 dark:text-white hover:bg-ltbg2 dark:hover:bg-dkbg2 transition-all'>
         <h3 className='text-lg lg:text-5xl'>{title}</h3>
-        <p className='text-base lg:text-xl'>{bodyText}</p>
+        <p className='text-base lg:text-xl truncate max-h-10'>{bodyText}</p>
         <a
           href={link}
-          className='text-sm underline underline-offset-4 transition-all  duration-300 hover:text-tertiary lg:text-lg'
+          className='text-sm text-tertiary hover:text-accent underline underline-offset-4 transition-all  duration-300 lg:text-2xl'
         >
           {linkText}
         </a>
