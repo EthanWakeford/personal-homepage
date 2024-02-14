26060af0de8f7443a7262fc698107917c6beece9
EthanWakeford <ethanwakeford@gmail.com>
2024-02-13 20:07:17 -0600

    added project gallery

--- src/components/ProjectGallery.tsx
+++ src/components/ProjectGallery.tsx
@@ -1,7 +1,7 @@
-import type { DocumentData } from 'firebase/firestore';
+import type { Project } from '../firebase';
 
 interface ProjectGalleryProps {
-  projects: DocumentData[];
+  projects: Project[];
   galleryHeader: string;
 }
 
@@ -18,17 +18,17 @@ const ProjectGallery: React.FC<ProjectGalleryProps> = ({
         <ul className='align-center grid grid-cols-1 text-center hover:cursor-pointer group-hover:shadow-inner sm:grid-cols-2'>
           {projects.map((project) => (
             <li
-              key={project.slug} // Use the slug as a unique key for each project
+              key={project.url_slug} // Use the slug as a unique key for each project
               className='card-zoom group relative z-10 flex h-64 bg-cover bg-center'
             >
               <a
                 className='flex size-full items-center justify-center  hover:scale-100'
-                href={`/${project.project_category.toLowerCase()}/${project.slug}`}
+                href={`/${project.project_type}/${project.url_slug}`}
               >
                 <div
                   className='card-zoom-image group-hover:zoom-in z-0 group-hover:brightness-50 lg:brightness-100'
                   style={{
-                    backgroundImage: `url(${project.hero_section.project_hero_photo})`,
+                    backgroundImage: `url(${project.hero_picture})`,
                   }}
                 ></div>
                 <span className='z-50 rounded bg-white bg-opacity-50 p-2 text-xl text-black transition-all duration-300 ease-in-out hover:bg-opacity-100 group-hover:opacity-100 lg:opacity-0 dark:bg-black dark:text-white'>
