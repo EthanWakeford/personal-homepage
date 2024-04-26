99601134b4b4e981dbecd6f2a0d2b9665becf439
EthanWakeford <ethanwakeford@gmail.com>
2024-04-26 15:01:50 -0500

    set up to use github app to authenticate to github API

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,26 +3,32 @@ import { initializeApp } from 'firebase-admin/app';
 import { onRequest } from 'firebase-functions/v2/https';
 import { info } from 'firebase-functions/logger';
 import { defineString } from 'firebase-functions/params';
+import { createAppAuth } from '@octokit/auth-app';
 import axios from 'axios';
 
 const GITHUB_REPO = defineString('GITHUB_REPO');
-const GITHUB_TOKEN = defineString('GITHUB_TOKEN');
+// const GITHUB_TOKEN = defineString('GITHUB_TOKEN');
 const WORKFLOW_ID = defineString('WORKFLOW_ID');
 const PROJECT_ID = defineString('PROJECT_ID');
 const QUEUE_NAME = defineString('QUEUE_NAME');
 const BUILD_URL = defineString('BUILD_URL');
+const APP_ID = defineString('APP_ID');
+const PRIVATE_KEY = defineString('PRIVATE_KEY');
+const INSTALLATION_ID = defineString('INSTALLATION_ID');
 const location = 'us-central1';
 
 initializeApp();
 
 export const buildMe = onRequest(async (req, res) => {
+  const installationToken = await createInstallationToken();
+
   const currentDate = new Date();
   const thisTimestamp = Math.floor(currentDate.getTime() / 1000);
 
   // run in 5 minutes
   const toRunTimestamp = thisTimestamp + 5 * 60;
 
-  await createBuildTask(toRunTimestamp)
+  await createBuildTask(toRunTimestamp, installationToken)
     .then(() => {
       res.send('success');
     })
@@ -42,7 +48,25 @@ export const buildOnWritten = onDocumentWritten('**/*', async (event) => {
     });
 });
 
-const createBuildTask = async (toRunTimestamp: number) => {
+async function createInstallationToken() {
+  const auth = createAppAuth({
+    appId: APP_ID.value(),
+    privateKey: PRIVATE_KEY.value(),
+  });
+
+  // Retrieve installation access token
+  const installationAuthentication = await auth({
+    type: 'installation',
+    installationId: INSTALLATION_ID.value(),
+  });
+
+  return installationAuthentication.token;
+}
+
+async function createBuildTask(
+  toRunTimestamp: number,
+  installationToken: string
+) {
   const { v2beta3 } = await import('@google-cloud/tasks');
   const tasksClient = new v2beta3.CloudTasksClient();
   const parent = tasksClient.queuePath(
@@ -51,10 +75,13 @@ const createBuildTask = async (toRunTimestamp: number) => {
     QUEUE_NAME.value()
   );
 
+  info(installationToken);
+
   const [tasklist] = await tasksClient.listTasks({ parent });
 
   if (tasklist.length > 0) {
     info('task debounced');
+    console.log(tasklist[0].httpRequest?.url);
 
     // clear to debounce the task
     for (const task of tasklist) {
@@ -78,7 +105,7 @@ const createBuildTask = async (toRunTimestamp: number) => {
       body: Buffer.from(JSON.stringify(body)).toString('base64'),
       headers: {
         Accept: 'application/vnd.github+json',
-        Authorization: `Bearer ${GITHUB_TOKEN.value()}`,
+        Authorization: `Bearer ${installationToken}`,
         'X-GitHub-Api-Version': '2022-11-28',
         'Content-Type': 'application/json',
       },
@@ -89,4 +116,4 @@ const createBuildTask = async (toRunTimestamp: number) => {
 
   info('task added');
   return response;
-};
+}
